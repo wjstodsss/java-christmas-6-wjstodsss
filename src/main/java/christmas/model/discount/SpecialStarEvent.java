9d27bf334eff587a9c7df127c8987b09e1eed5af
Finn <wjstodsss@naver.com>
2023-11-13 06:02:21 +0900

    feat : 특별 할인을 확인 할 수 있다./ 테스트 완료

---
+++ src/main/java/christmas/model/discount/SpecialStarEvent.java
@@ -0,0 +1,9 @@
+package christmas.model.discount;
+
+public class SpecialStarEvent {
+    public static final int SPECIAL_STAR_DISCOUNT = 1000;
+
+    public static int getSpecialStarDiscount() {
+        return SPECIAL_STAR_DISCOUNT;
+    }
+}
