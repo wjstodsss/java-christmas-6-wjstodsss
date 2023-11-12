477da6b5e0b5ca8cd68ad2ff501990190673d802
Finn <wjstodsss@naver.com>
2023-11-12 17:50:42 +0900

    test : 주문 메뉴 이름을 SET에 담기 테스트 완료

--- src/main/java/christmas/util/Calculator.java
+++ src/main/java/christmas/util/Calculator.java
@@ -1,18 +1,42 @@
 package christmas.util;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-    public static int calculateSum(String input) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(input);
+    public static Set<String> extractWords(String input) {
+        return processMatches(input, "[가-힣]+");
+    }
 
+    public static int calculateSumOfMatches(String input) {
+        Matcher numberMatcher = createMatcher(input, "\\d+");
+        return sumOfMatches(numberMatcher);
+    }
+
+    private static Matcher createMatcher(String input, String regex) {
+        return Pattern.compile(regex).matcher(input);
+    }
+
+    private static Set<String> processMatches(String input, String regex) {
+        Matcher matcher = createMatcher(input, regex);
+        Set<String> matches = new HashSet<>();
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        return matches;
+    }
+
+    private static int sumOfMatches(Matcher matcher) {
         int sum = 0;
         while (matcher.find()) {
-            sum += Integer.parseInt(matcher.group());
+            sum += parseToInt(matcher.group());
         }
-
         return sum;
+    }
+
+    private static int parseToInt(String str) {
+        return Integer.parseInt(str);
     }
 }
--- src/test/java/christmas/util/CalculatorTest.java
+++ src/test/java/christmas/util/CalculatorTest.java
@@ -3,12 +3,27 @@ package christmas.util;
 import christmas.model.menu.MainDish;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @Test
     void 주문_메뉴의_총_개수1(){
         String orderMenu = "티본스테이크-3,양송이수프-1,아이스크림-5";
-        assertEquals(9, Calculator.calculateSum(orderMenu));
+        assertEquals(9, Calculator.calculateSumOfMatches(orderMenu));
+    }
+
+    @Test
+    public void 주문_메뉴_이름을_SET에_담기() {
+        String input = "티본스테이크-3,양송이수프-1,아이스크림-5";
+        Set<String> expectedKoreanWords = new HashSet<>();
+        expectedKoreanWords.add("티본스테이크");
+        expectedKoreanWords.add("양송이수프");
+        expectedKoreanWords.add("아이스크림");
+
+        Set<String> koreanWords = Calculator.extractWords(input);
+        assertEquals(expectedKoreanWords, koreanWords);
     }
 }
