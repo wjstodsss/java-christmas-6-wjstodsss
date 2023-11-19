56dc446c317900f31f9b8310d065937530c5784a
Finn <wjstodsss@naver.com>
2023-11-19 16:52:01 +0900

    refactor : AppIODataController에서 할인이벤트 부분을 분리한다.

--- docs/README.md
+++ docs/README.md
@@ -140,7 +140,7 @@
     - [x] refactor : AppIODataController에서 예약 정보 부분을 분리한다.
         - [x] refactor : get은 반환값을 의미한다고 해서 OutputView클래스 내의 메서드명의 서술어를 print로 변경함
     - [x] refactor : AppIODataController에서 주문 정보 부분을 분리한다.
-    - [ ] refactor : AppIODataController에서 할인이벤트 부분을 분리한다.
+    - [x] refactor : AppIODataController에서 할인이벤트 부분을 분리한다.
     - [ ] refactor : AppIODataController에서 결제 및 배지 리워드 부분을 분리한다.
     
     
--- src/main/java/christmas/controller/AppIODataController.java
+++ src/main/java/christmas/controller/AppIODataController.java
@@ -1,156 +1,41 @@
 package christmas.controller;
 
-import christmas.model.counter.PointOfSale;
 import christmas.view.OutputView;
-import java.time.DayOfWeek;
 import static christmas.model.event.BadgeAwardEvent.awardBadge;
-import static christmas.model.event.ChristmasDDay.dDayDiscount;
-import static christmas.model.event.FreeChampagneEvent.*;
-import static christmas.model.event.MenuDiscountEvent.menuDiscount;
-import static christmas.model.event.SpecialStarEvent.specialDiscount;
-import static christmas.util.Date.getDayOfWeekFromDate;
+
 
 public class AppIODataController {
     private String inputDate;
     private String inputOrder;
-    private final OutputView outputView = new OutputView();
-    private PointOfSale pointOfSale = new PointOfSale();
     private int totalAmount = 0;
-    private int totalDiscountAmount = 0;
-    private final int MIN_AMOUNT = 10000;
-    private final String NOTHING = "없음";
+    private final OutputView outputView = new OutputView();
 
+    private int totalDiscountAmount = 0;
 
     public AppIODataController() {
         visitInformationController();
+        orderInformationController();
+        discountController();
 
-        orderInformationController(inputOrder);
-
-        freeAwardView(totalAmount);
-
-        eventEntry(totalAmount);
-        benefitsDetailsValue();
-        benefitsDetailView();
-
-        totalDiscountAmountOutputView();
         expectedDiscountedPaymentAmount();
         decemberBadgeAward();
     }
 
-    public void visitInformationController() {
+    private void visitInformationController() {
         VisitInformationController visitInformationController = new VisitInformationController();
         this.inputDate = visitInformationController.getDateInput();
         this.inputOrder = visitInformationController.getOrderInput();
     }
 
-    public void orderInformationController(String inputOrder) {
+    private void orderInformationController() {
         OrderInformationController orderInformationController = new OrderInformationController(inputOrder);
-        this.totalAmount=orderInformationController.calculateTotalAmount();
+        this.totalAmount = orderInformationController.calculateTotalAmount();
     }
 
-    public void eventEntry(int totalAmount) {
-        if (totalAmount < MIN_AMOUNT) {
-            outputView.printBoundaryEmptyLine();
-            outputView.printBenefitsDetails();
-            System.out.println(NOTHING);
-        }
-    }
+    private void discountController() {
+        DiscountController discountController = new DiscountController(totalAmount, inputDate, inputOrder);
+        this.totalDiscountAmount = discountController.totalDiscountAmount();
 
-
-
-    private void freeAwardView(int totalAmount) {
-        outputView.printBoundaryEmptyLine();
-        outputView.printFreeAward();
-        outputView.printFreeAwardFormat(freeChampagneEvent(totalAmount));
-    }
-
-
-    private String freeChampagneEvent(int totalAmount) {
-        if (validateFreeChampagne(totalAmount)) {
-            return freeChampane() + " 1개";
-        }
-        return "없음";
-    }
-
-    private int dDayDiscount = 0;
-    private int weekDiscount = 0;
-    private int specialDiscount = 0;
-    private int freeAward = 0;
-
-    private void benefitsDetailsValue() {
-        while (totalAmount >= MIN_AMOUNT) {
-            outputView.printBoundaryEmptyLine();
-            outputView.printBenefitsDetails();
-            dDayDiscount = dDayDiscount(inputDate);
-            weekDiscount = menuDiscount(inputDate, inputOrder);
-            specialDiscount = specialDiscount(inputDate);
-            freeAward = freeChampagneDiscount(totalAmount);
-            break;
-        }
-    }
-
-    private void benefitsDetailView() {
-        dDayDiscountDetailView(dDayDiscount);
-        weekendDiscountDetailView(weekDiscount, inputDate);
-        weekdayDiscountDetailView(weekDiscount, inputDate);
-        specialDiscountDetailView(freeAward);
-        freeAwardDetailView(specialDiscount);
-    }
-
-    private void dDayDiscountDetailView(int dDayDiscount) {
-        if (dDayDiscount != 0) {
-            outputView.printDDayDiscount(dDayDiscount);
-        }
-    }
-
-    private void weekendDiscountDetailView(int weekDiscount, String inputDate) {
-        DayOfWeek dayOfWeek = getDayOfWeekFromDate(inputDate);
-        if (weekDiscount != 0 && dayOfWeek.equals(DayOfWeek.FRIDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY)) {
-            outputView.printWeekendDiscount(weekDiscount);
-        }
-    }
-
-    private void weekdayDiscountDetailView(int weekDiscount, String inputDate) {
-        DayOfWeek dayOfWeek = getDayOfWeekFromDate(inputDate);
-        if (weekDiscount != 0 && !dayOfWeek.equals(DayOfWeek.FRIDAY) && !dayOfWeek.equals(DayOfWeek.SATURDAY)) {
-            outputView.printWeekdayDiscount(weekDiscount);
-        }
-    }
-
-    private void specialDiscountDetailView(int freeAward) {
-        if (freeAward != 0) {
-            outputView.printAwardEvent(freeAward);
-        }
-    }
-
-    private void freeAwardDetailView(int specialDiscount) {
-        if (specialDiscount != 0) {
-            outputView.printSpecialDiscount(specialDiscount);
-        }
-    }
-
-    private void totalDiscountAmountOutputView() {
-        outputView.printBoundaryEmptyLine();
-        outputView.printTotalBenefitsAmount();
-        totalDiscountAmount();
-        totalDiscountAmountZeroView();
-        totalDiscountAmountView();
-    }
-
-    private void totalDiscountAmount() {
-        totalDiscountAmount = dDayDiscount + weekDiscount + specialDiscount + freeAward;
-    }
-
-    private void totalDiscountAmountZeroView() {
-        if (totalDiscountAmount == 0) {
-            outputView.printTotalDiscountAmountZeroFormat();
-        }
-    }
-
-    private void totalDiscountAmountView() {
-        if (totalDiscountAmount != 0) {
-            outputView.printTotalDiscountAmountFormat(totalDiscountAmount);
-        }
     }
 
     private void expectedDiscountedPaymentAmount() {
